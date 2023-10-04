432e7dbc87c4d0c6d1b95ceb1af99c15a90fe3cf
ZduardoPereira <zduardofernandes@gmail.com>
2023-10-03 22:16:24 -0300

    Arrumando os links para ser puxado pelo flask

--- src/app.py
+++ src/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return render_template('')
+@app.route('/')
+def home():
+     return render_template('index.html')
 
 @app.route('/cadastro')
 def cadastro():
