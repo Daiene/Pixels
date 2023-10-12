ff62f6396030083d66011bf541078569461e3de8
diogopalharini <diogo.palharini@gmail.com>
2023-10-11 22:44:18 -0300

    Estrutura do menu de opcoes do usuario logado

--- src/static/tela_perfil.js
+++ src/static/tela_perfil.js
@@ -1,12 +1,13 @@
-const botao = document.getElementById('perfil_nome');
-const opcoes = document.getElementsByClassName('tela_perfil_nome');
 
-botao.addEventListener('click', (e) => {
-    e.preventDefault(); // Evita que o link siga para "#" (URL vazia)
-
-    if (opcoes.style.display === 'none' || opcoes.style.display === '') {
-        opcoes.style.display = 'block';
-    } else {
-        opcoes.style.display = 'none';
-    }
-});
+window.onload = function() {
+    let botao = document.getElementById('perfil_nome');
+    
+    botao.addEventListener('click', () => {
+        let opcoes = document.getElementsByClassName('tela_perfil_nome')[0];
+        if (opcoes.style.display === 'none' || opcoes.style.display === '') {
+            opcoes.style.display = 'block';
+        } else {
+            opcoes.style.display = 'none';
+        }
+    });
+    };
