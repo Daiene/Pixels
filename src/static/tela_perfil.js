e1b94d3aa9a1ceb9bd761e54cdee2a16cacceef9
diogopalharini <diogo.palharini@gmail.com>
2023-10-13 17:27:15 -0300

    Adicionando fundo preto ao clicar no nome(user)

--- src/static/tela_perfil.js
+++ src/static/tela_perfil.js
@@ -3,11 +3,14 @@ window.onload = function() {
     let botao = document.getElementById('perfil_nome');
     
     botao.addEventListener('click', () => {
+        let escuro = document.getElementById('fundo-escuro');
         let opcoes = document.getElementsByClassName('tela_perfil_nome')[0];
         if (opcoes.style.display === 'none' || opcoes.style.display === '') {
             opcoes.style.display = 'block';
+            escuro.style.display = 'block';
         } else {
             opcoes.style.display = 'none';
+            escuro.style.display = 'none';
         }
     });
     };
