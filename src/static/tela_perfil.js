c7402cca10c398820d0208cc33b7d41b0d1d2f96
diogopalharini <diogo.palharini@gmail.com>
2023-11-04 17:16:39 -0300

    correção no js para clicar na div e sair da telinha do perfil

--- src/static/tela_perfil.js
+++ src/static/tela_perfil.js
@@ -1,11 +1,10 @@
-
 window.onload = function() {
     let botao = document.getElementById('perfil_nome');
     let botao_es = document.getElementById('click_escuro');
-    
+    let escuro = document.getElementById('fundo-escuro');
+    let opcoes = document.getElementsByClassName('tela_perfil_nome')[0];
+
     botao.addEventListener('click', () => {
-        let escuro = document.getElementById('fundo-escuro');
-        let opcoes = document.getElementsByClassName('tela_perfil_nome')[0];
         if (opcoes.style.display === 'none' || opcoes.style.display === '') {
             opcoes.style.display = 'block';
             escuro.style.display = 'block';
@@ -14,8 +13,9 @@ window.onload = function() {
             escuro.style.display = 'none';
         }
     });
+
     botao_es.addEventListener('click', () => {
         opcoes.style.display = 'none';
         escuro.style.display = 'none';
-    })
+    });
 };
